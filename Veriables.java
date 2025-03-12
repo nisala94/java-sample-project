855a8220e75df497987abb05fc19714100b79778
nisala94 <nisalaweerasekara@gmail.com>
2025-03-13 02:08:50 +0530

    Types of Veriables java

---
+++ Veriables.java
@@ -0,0 +1,14 @@
+class Veriables
+{
+    static int a=10;/*Static veriable */
+    int b=20;/*Instance Veriable */
+    public static void main(String args[])
+    {
+        int c=30;/*Local Veriable */
+        System.out.println("This is a Static Veriable="+a);
+        Veriables vr=new Veriables();
+        System.out.println("This is a Instance Veriable="+vr.b);
+        System.out.println("This is a Local Veriable="+c);
+    }
+
+}
