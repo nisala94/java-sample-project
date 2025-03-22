2f948b30fd3af7d3dddc90761d9c977381375e02
nisala94 <nisalaweerasekara@gmail.com>
2025-03-23 00:09:48 +0530

    Exception handling demo modified

--- ExceptionHandlingDemo.java
+++ ExceptionHandlingDemo.java
@@ -10,9 +10,63 @@ class Ex{
         {
             System.out.println(e);
         }
+        System.out.println("This was run after ArithmeticException handled");
+    }
+    void adisplay()
+    {
+        try
+        {
+            int a[] =new int[5];
+            a[6]=5;
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("This was run after ArrayIndexOutOfBoundsException handled");
     }
 
+    void bdisplay()
+    {
+        try
+        {
+            String str="Hello";
+            int num=Integer.parseInt(str);
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("This was run after NumberFormatException handled");
+    }
 
+    void cdisplay()
+    {
+        try
+        {
+            String str=null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("This was run after NullPointerException handled");
+    }
+
+    void ddisplay()
+    {
+        try
+        {
+            int a[] =new int[5];
+            a[6]=5;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("This was run after Exception handled");
+    }
 
 }
 
@@ -21,6 +75,10 @@ class ExceptionHandlingDemo {
     {
         Ex obj =new Ex();
         obj.display();
+        obj.adisplay();
+        obj.bdisplay();
+        obj.cdisplay();
+        obj.ddisplay();
     }
     
 }
