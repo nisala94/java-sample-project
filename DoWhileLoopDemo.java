50031f3498cd2fa59a0826765382a8f2c95393e5
nisala94 <nisalaweerasekara@gmail.com>
2025-04-05 01:57:33 +0530

    Dowhileloop demo

---
+++ DoWhileLoopDemo.java
@@ -0,0 +1,15 @@
+public class DoWhileLoopDemo {
+
+    public static void main(String[] args) {
+        
+        //2.do while loop
+        //ex-01-1to10 number
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+    }
+    
+}
